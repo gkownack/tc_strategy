d918d0fe3cb45701d760ceae77550236509e5528
billy <wtwood@andrew.cmu.edu>
2014-03-22 16:21:18 -0400

    done testing git for instructions

--- game/game_main.py
+++ game/game_main.py
@@ -5,9 +5,8 @@ from pygame.locals import *
 # Initialization routines
 pygame.init()
 config.init()
-print "fak u billy"
+
 while 1:
-    "I'm a boss!"
     for event in pygame.event.get():
         config.STATE.handle_event(event);
     config.STATE.update()
